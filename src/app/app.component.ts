d30a7542618a75040085f801b42a295f3ba08a2d
Gabriel Antognoli <gabriel_antognoli@hotmail.com>
2025-09-06 19:13:30 -0300

    feat: add certificado form component with styling and functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { NavbarComponent } from "./_components/navbar/navbar.component";
 import { CommonModule } from '@angular/common';
 import { BaseUiComponent } from "./_components/base-ui/base-ui.component";
 import { CertificadosComponent } from "./pages/certificados/certificados.component";
+import { CertificadoFormComponent } from "./pages/certificado-form/certificado-form.component";
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, CommonModule, BaseUiComponent, CertificadosComponent],
+  imports: [NavbarComponent, CommonModule, BaseUiComponent, CertificadosComponent, CertificadoFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
