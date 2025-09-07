7a4c01aec86cba2ca08f2d69e63a40d07696df01
Gabriel Antognoli <gabriel_antognoli@hotmail.com>
2025-09-06 22:14:17 -0300

    feat: add certificate page with styling and SVG assets
    - Implemented the certificate component with HTML structure and CSS styling.
    - Added SVG assets for certificate signature and logo.
    - Included a back button and secondary button in the navigation.
    - Styled the certificate layout, including the student's name and issued date.
    - Integrated Google Fonts for typography enhancements.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { BaseUiComponent } from "./_components/base-ui/base-ui.component";
 import { CertificadosComponent } from "./pages/certificados/certificados.component";
 import { CertificadoFormComponent } from "./pages/certificado-form/certificado-form.component";
+import { CertificadoComponent } from "./pages/certificado/certificado.component";
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, CommonModule, BaseUiComponent, CertificadosComponent, CertificadoFormComponent],
+  imports: [NavbarComponent, CommonModule, BaseUiComponent, CertificadosComponent, CertificadoFormComponent, CertificadoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
