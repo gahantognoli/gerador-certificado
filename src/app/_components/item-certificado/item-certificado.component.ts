0f4fe368dad4e1a463c7e7d73c481d85839b7722
Gabriel Antognoli <gabriel_antognoli@hotmail.com>
2025-09-07 12:52:25 -0300

    feat: implement routing for certificados and update button functionalities

--- src/app/_components/item-certificado/item-certificado.component.ts
+++ src/app/_components/item-certificado/item-certificado.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-import { SecondaryButtonComponent } from "../secondary-button/secondary-button.component";
+import { SecondaryButtonComponent } from '../secondary-button/secondary-button.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-item-certificado',
   imports: [SecondaryButtonComponent],
   templateUrl: './item-certificado.component.html',
-  styleUrl: './item-certificado.component.css'
+  styleUrl: './item-certificado.component.css',
 })
 export class ItemCertificadoComponent {
+  id: string = '1';
 
+  constructor(private router: Router) {}
+
+  redirecionarCertificado() {
+    this.router.navigate(['/certificados', this.id]);
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from "./_components/navbar/navbar.component";
+import { NavbarComponent } from './_components/navbar/navbar.component';
 import { CommonModule } from '@angular/common';
-import { BaseUiComponent } from "./_components/base-ui/base-ui.component";
-import { CertificadosComponent } from "./pages/certificados/certificados.component";
-import { CertificadoFormComponent } from "./pages/certificado-form/certificado-form.component";
-import { CertificadoComponent } from "./pages/certificado/certificado.component";
+import { BaseUiComponent } from './_components/base-ui/base-ui.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, CommonModule, BaseUiComponent, CertificadosComponent, CertificadoFormComponent, CertificadoComponent],
+  imports: [
+    NavbarComponent,
+    CommonModule,
+    BaseUiComponent,
+    RouterOutlet
+],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'gerador-certificado';
