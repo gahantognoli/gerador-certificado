5a0978027aecad1566e360a796c848632cc9540b
Gabriel Antognoli <gabriel_antognoli@hotmail.com>
2025-09-13 10:40:07 -0300

    feat: refactor certificado form to use a structured Certificado interface; enhance activity management and form validation

--- src/app/pages/certificado-form/certificado-form.component.ts
+++ src/app/pages/certificado-form/certificado-form.component.ts
@@ -3,23 +3,47 @@ import { PrimaryButtonComponent } from '../../_components/primary-button/primary
 import { SecondaryButtonComponent } from '../../_components/secondary-button/secondary-button.component';
 import { FormsModule, NgModel } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { Certificado } from '../../interfaces/certificado';
 
 @Component({
   selector: 'app-certificado-form',
-  imports: [SecondaryButtonComponent, PrimaryButtonComponent, FormsModule, CommonModule],
+  imports: [
+    SecondaryButtonComponent,
+    PrimaryButtonComponent,
+    FormsModule,
+    CommonModule,
+  ],
   templateUrl: './certificado-form.component.html',
   styleUrl: './certificado-form.component.css',
 })
 export class CertificadoFormComponent {
-  nome: string = '';
   atividade: string = '';
-  atividades: string[] = [];
+  certificado: Certificado = {
+    nome: '',
+    atividades: [],
+  };
 
   campoInvalido(control: NgModel): boolean {
     return (control.invalid && control.touched) ?? false;
   }
 
   formValido(): boolean {
-    return this.nome.trim().length > 0 && this.atividades.length > 0;
+    return this.certificado.nome.trim().length > 0 && this.certificado.atividades.length > 0;
+  }
+
+  adicionarAtividade(): void {
+    this.certificado.atividades.push(this.atividade.trim());
+    this.atividade = '';
+  }
+
+  excluirAtividade(index: number): void {
+    this.certificado.atividades.splice(index, 1);
+  }
+
+  submit(): void {
+    if(!this.formValido()) {
+      return;
+    }
+    console.log(this.certificado);
   }
 }
